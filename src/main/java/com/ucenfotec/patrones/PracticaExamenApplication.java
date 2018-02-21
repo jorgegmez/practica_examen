6d7e09639153862849aab232c45cebc1b2201a11
Jorge Gomez <jgomezm@ucenfotec.ac.cr>
2018-02-20 18:41:00 -0600

    Initial commit

---
+++ src/main/java/com/ucenfotec/patrones/PracticaExamenApplication.java
@@ -0,0 +1,12 @@
+package com.ucenfotec.patrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaExamenApplication.class, args);
+	}
+}
